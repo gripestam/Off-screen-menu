56c6bf69512615d433a6c849cfd8235a7b020406
lepardman <erik.gripestam@gmail.com>
2014-09-10 11:40:00 +0200

    initial commit
    base

---
+++ script.js
@@ -0,0 +1,35 @@
+(function($) {
+
+	$(function() {
+		
+		$('body').toggleMenu();
+			
+	});
+
+})(jQuery);
+
+
+
+
+
+
+(function($) {
+
+	$.fn.toggleMenu = function() {
+		
+		var trigger = $('.menu-trigger');
+		
+		trigger.click(function() {
+			
+			if ( $('body').hasClass('menu-active') ) {
+				$('body').removeClass('menu-active');
+			} else {
+				$('body').addClass('menu-active');
+			}
+			
+		});
+		
+	
+	}
+
+})(jQuery);
